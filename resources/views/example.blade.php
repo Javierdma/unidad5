8b54f876e8c38cd1db31fccc380e8593a9e1406e
Javierdma <javierestebandominguez@outlook.com>
2023-11-03 22:33:42 -0700

    Implementeción de rutas para operaciones y saludos

---
+++ resources/views/example.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1 style="font-size: 50px;">Hooola <?=$name?></h1>
+
+    <img src="https://media.tenor.com/QtL7JnuocPIAAAAd/ni%C3%B1a-japonesa.gif" >
+    
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/suma/{n1}/{n2}', function (string $n1, string $n2) {
+    return $n1+$n2;
+})->where(['n1'=>'[0-9]+','n2'=>'[0-9]+']);
+Route::get('/resta/{n1}/{n2}', function (string $n1, string $n2) {
+    return $n1-$n2;
+})->where(['n1'=>'[0-9]+','n2'=>'[0-9]+']);
+Route::get('/multiplicacion/{n1}/{n2}', function (string $n1, string $n2) {
+    return $n1*$n2;
+})->where(['n1'=>'[0-9]+','n2'=>'[0-9]+']);
+Route::get('division/{n1}/{n2}', function (string $n1, string $n2) {
+    return $n1/$n2;
+})->where(['n1'=>'[0-9]+','n2'=>'[0-9]+']);
+Route::get('/hola/{name}/{lastname?}', function (string $name, ?string $lastname = "Doe") {
+    return 'Hola '.$name.' '.$lastname;
+})->where(['name'=>'[a-z]+','lastname'=>'[a-z]+']);
+Route::get('/saludo/{name}', function (string $name) {
+    return view('example',['name'=>$name]);
+})->where(['name'=>'[a-z]+']);
